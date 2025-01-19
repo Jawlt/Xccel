bd70c0638b69b3a39007304df70d2790c944edc1
Anais <anaiszed447@gmail.com>
2025-01-18 20:59:48 -0500

    Changed manifest file

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,1 +1,16 @@
 console.log("React Chrome Extension content script loaded!");
+
+
+
+//document.body.style.backgroundColor = 'blue';
+
+// see the note below on how to choose currentWindow or lastFocusedWindow
+//chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+  //  let url = tabs[0].url;
+   // console.log(url)
+//});
+
+chrome.tabs.query(null,function(tab) {
+    var tablink = tab.url;
+    console.log(tablink)
+});
